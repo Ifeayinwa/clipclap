f7852e2ac2b3113e96e2e33a58c24e43f9153085
Ifeayinwa <138693167+Ifeayinwa@users.noreply.github.com>
2025-08-24 20:57:21 +0100

    Update models.py

--- videos/models.py
+++ videos/models.py
@@ -2,14 +2,32 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 import uuid
-from storages.backends.azure_storage import AzureStorage 
+from storages.backends.azure_storage import AzureStorage
+from django.core.exceptions import ValidationError
+import os
+
 User = get_user_model()
+
+def validate_video_file_extension(value):
+    ext = os.path.splitext(value.name)[1].lower()
+    valid_extensions = ['.mp4', '.mov', '.avi', '.wmv', '.flv', '.webm']
+    if ext not in valid_extensions:
+        raise ValidationError('Unsupported file format. Please upload a video file.')
 
 class Video(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='videos')
-    video_file = models.FileField(upload_to='videos/', storage=AzureStorage())
-    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True, storage=AzureStorage())
+    video_file = models.FileField(
+        upload_to='videos/%Y/%m/%d/', 
+        storage=AzureStorage(),
+        validators=[validate_video_file_extension]
+    )
+    thumbnail = models.ImageField(
+        upload_to='thumbnails/%Y/%m/%d/', 
+        blank=True, 
+        null=True, 
+        storage=AzureStorage()
+    )
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
